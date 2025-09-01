0163783a73b3836375ae9bbe7574ca73afb5890e
Thatiane <thatiane.ssantana@gmail.com>
2025-09-01 16:38:46 -0300

    Adiciona Pasta Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
   return (
